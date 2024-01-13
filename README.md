6520e9abc95f9bf7cc01100bbdef451fa67759cd
lburbano <lburbano@ucsc.com>
2024-01-12 17:56:43 -0800

    First push

---
+++ README.md
@@ -0,0 +1,6 @@
+# Repository for the advanced computer security class - Winter 2023
+
+The project uses the simulator [CybORG](https://github.com/cage-challenge/cage-challenge-2/tree/main). Please follow the [instructions](\https://github.com/cage-challenge/cage-challenge-2/tree/main/CybORG) to install the simulator.
+
+## Requirements
+
---
+++ red_train.py
@@ -0,0 +1,56 @@
+# Implementation of the red evaluation script
+# This script is used to evaluate the performance of a red agent against a blue agent
+# The blue agent is https://github.com/john-cardiff/-cyborg-cage-2.git
+# Modified by Prof. H. Sasahara
+import inspect
+import time
+
+from CybORG import CybORG, CYBORG_VERSION
+from CybORG.Agents import B_lineAgent, SleepAgent
+from CybORG.Agents.SimpleAgents.Meander import RedMeanderAgent
+from Wrappers.ChallengeWrapper2 import ChallengeWrapper2
+from Agents.MainAgent import MainAgent
+from Agents.WrappedAgent import WrappedBlueAgent
+import random
+
+MAX_EPS = 100
+agent_name = 'Red'
+random.seed(153)
+
+
+# changed to ChallengeWrapper2
+def wrap(env):
+    return ChallengeWrapper2(env=env, agent_name=agent_name)
+
+if __name__ == "__main__":
+    cyborg_version = CYBORG_VERSION
+    scenario = 'Scenario2'
+    # commit_hash = get_git_revision_hash()
+    
+
+    
+    # Load scenario
+    path = str(inspect.getfile(CybORG))
+    path = path[:-10] + f'/Shared/Scenarios/{scenario}.yaml'
+    # Load Red agent
+    red_agent = RedMeanderAgent()
+    # Load blue agent
+    blue_agent = WrappedBlueAgent
+    # Set up environment with blue agent running in the background and 
+    # red agent as the main agent
+    cyborg = CybORG(path, 'sim', agents={'Blue': blue_agent})
+    env = ChallengeWrapper2(env=cyborg, agent_name="Red")
+
+    # red_agent = TODO: load red agent
+    max_episodes = 1
+    max_timesteps = 1
+    for i_episode in range(1, max_episodes + 1):
+        state = env.reset()
+        time_step = 0
+        for t in range(max_timesteps):
+            time_step += 1
+            action = 1 # TODO implement red agent
+            state, reward, done, _ = env.step(action)
+            # TODO: implement red agent training
+
+    
